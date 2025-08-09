e2c2bd724d05a5fc585fb052c419cc5b334cd353
Ruby bnf <r.barnerfontyn@gmail.com>
2025-08-09 23:20:45 +0200

    Intial setting

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./sections/Navbar";
+
+function App() {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar />
+    </main>
+  );
+}
+
+export default App;
