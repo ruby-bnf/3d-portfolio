feb2937c905ec23d08c391d05da209574ba00ab6
Ruby bnf <r.barnerfontyn@gmail.com>
2025-08-11 21:43:50 +0200

    style fixes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -246,7 +246,12 @@ export const myProjects = [
   },
 ];
 
-export const calculateSizes = (isSmall: any, isMobile: any, isTablet: any) => {
+//use to calcualte zise of objects base of screen size
+export const calculateSizes = (
+  isSmall: unknown,
+  isMobile: unknown,
+  isTablet: unknown
+) => {
   return {
     deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
     deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,33 +4,21 @@ import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
 import { HackerRoom } from "../components/HackerRoom";
 import CanvasLoader from "../components/CanvasLoader";
-import { Leva, useControls } from "leva";
+import { Leva } from "leva";
 import { calculateSizes } from "../constants";
 import { useMediaQuery } from "react-responsive";
 
 export const Hero = () => {
-  // leva - tools to find the scales for the three 3 objects
-  // const controls = useControls("HackerRoom", {
-  //   positionX: { value: 2.5, min: -10, max: 10 },
-  //   positionY: { value: 2.5, min: -10, max: 10 },
-  //   positionZ: { value: 2.5, min: -10, max: 10 },
-
-  //   rotationX: { value: 0, min: -10, max: 10 },
-  //   rotationY: { value: 0, min: -10, max: 10 },
-  //   rotationZ: { value: 0, min: -10, max: 10 },
-
-  //   scale: { value: 1, min: 0.1, max: 10 },
-  // });
+  const isSmallDevice = useMediaQuery({ maxWidth: 440 });
 
   const isMobile = useMediaQuery({ maxWidth: 768 });
-  // const isSmall = useMediaQuery({ maxWidth: 440 });
-  // const isMobile = useMediaQuery({ maxWidth: 768 });
-  // const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 102 });
 
-  // const sizes = calculateSizes(isSmall, isMobile, isTablet);
+  const sizes = calculateSizes(isSmallDevice, isMobile, isTablet);
+
   return (
     <div className="min-h-screen w-full flex flex-col relative ">
-      <div className="w-full mx-auto flex flex-col sm:mt-6 mt-20 c-space gap-3">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
           Hi! Im Ruby<span className="waving-hand">👋</span>
         </p>
@@ -46,9 +34,9 @@ export const Hero = () => {
             {/* To hide controller */}
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
             <HackerRoom
-              position={[8.5, -8, 0.8]}
+              position={sizes.deskPosition}
               rotation={[0, -Math.PI, 0]}
-              scale={isMobile ? 0.1 : 10}
+              scale={sizes.deskScale}
             />
 
             <ambientLight intensity={1} />
